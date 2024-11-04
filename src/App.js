6887465cfdce687f08e04a8bec0428b85112787f
berkaysson <berkaysonel85@gmail.com>
2024-11-05 01:09:24 +0300

    feature: add home page to implement infinite scroll and display for products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import HomePage from "./pages/home-page";
+import Container from "@mui/material/Container";
+import { ProductProvider } from "./contexts/product-context";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ProductProvider>
+        <Container maxWidth="lg" className="app">
+          <HomePage />
+        </Container>
+      </ProductProvider>
+    </QueryClientProvider>
   );
 }
 
