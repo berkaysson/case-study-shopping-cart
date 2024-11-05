9b641aa19e977202f6310acab488012bb46e3721
berkaysson <berkaysonel85@gmail.com>
2024-11-05 19:50:54 +0300

    refactor: update product list item

--- src/components/product-item/product-item-image.jsx
+++ src/components/product-item/product-item-image.jsx
@@ -1,39 +1,51 @@
-import { AddShoppingCart } from "@mui/icons-material";
-import { Box, Fab, Tooltip } from "@mui/material";
+import { RemoveShoppingCart, ShoppingCart } from "@mui/icons-material";
+import { Box, Fab } from "@mui/material";
 import React from "react";
 import Image from "../image/image";
 
-export const ProductItemImage = ({ product, handleAddToCart }) => {
+export const ProductItemImage = ({
+  product,
+  handleAddToCart,
+  isInCart,
+  handleRemoveFromCart,
+}) => {
   return (
     <Box sx={{ position: "relative", p: 1 }}>
       <Fab
-        color="primary"
+        color={isInCart ? "error" : "primary"}
         size="medium"
         className="add-cart-btn"
-        onClick={handleAddToCart}
+        onClick={isInCart ? handleRemoveFromCart : handleAddToCart}
         sx={{
           right: 16,
           bottom: 24,
           zIndex: 9,
           opacity: 0.8,
           position: "absolute",
+          "&:hover": {
+            opacity: 1,
+          },
         }}
       >
-        <AddShoppingCart sx={{ width: 24, height: 24 }} />
+        {isInCart ? (
+          <>
+            <RemoveShoppingCart sx={{ width: 24, height: 24 }} />
+          </>
+        ) : (
+          <ShoppingCart sx={{ width: 24, height: 24 }} />
+        )}
       </Fab>
 
-      <Tooltip placement="bottom-end">
-        <Image
-          alt={product.name}
-          src={product.imageUrl}
-          ratio="1/1"
-          sx={{
-            borderRadius: 1.5,
-            opacity: 0.9,
-            filter: "grayscale(0.5)",
-          }}
-        />
-      </Tooltip>
+      <Image
+        alt={product.name}
+        src={product.imageUrl}
+        ratio="1/1"
+        sx={{
+          borderRadius: 1.5,
+          opacity: 0.9,
+          filter: "grayscale(0.5)",
+        }}
+      />
     </Box>
   );
 };
--- src/components/product-item/product-item.jsx
+++ src/components/product-item/product-item.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Card, Typography, Stack, Box, Rating } from "@mui/material";
+import {
+  Card,
+  Typography,
+  Stack,
+  Box,
+  Rating,
+  Tooltip,
+  useTheme,
+} from "@mui/material";
 import { useCartContext } from "../../hooks/use-cart-context";
 import { ProductItemImage } from "./product-item-image";
 import { useToast } from "../../hooks/use-toast";
@@ -7,7 +15,8 @@ import { animated } from "react-spring";
 import { useScrollAnimation } from "../../hooks/use-scroll-animation";
 
 const ProductItem = ({ product }) => {
-  const { addItemToCart } = useCartContext();
+  const { addItemToCart, cartItems, removeItemFromCart } = useCartContext();
+  const theme = useTheme();
 
   const toast = useToast();
   const spring = useScrollAnimation();
@@ -17,64 +26,89 @@ const ProductItem = ({ product }) => {
     toast.showToast("Added to Cart", "success", 800);
   };
 
+  const handleRemoveFromCart = () => {
+    removeItemFromCart(product.id);
+    toast.showToast("Removed from Cart", "success", 800);
+  };
+
+  const isInCart = cartItems.some((item) => item.id === product.id);
+
   return (
     <animated.div style={spring}>
-      <Card
-        sx={{
-          backgroundColor: "background.paper",
-          "&:hover": {
-            boxShadow: 4,
-            transform: "translateY(-2px)",
-          },
-          width: 1,
-          maxWidth: 380,
-          transition: "all 0.2s ease-in-out",
-          mx: "auto",
-        }}
-      >
-        <ProductItemImage product={product} handleAddToCart={handleAddToCart} />
+      <Tooltip placement="top-start" title={product.name}>
+        <Card
+          sx={{
+            backgroundColor: "background.paper",
+            "&:hover": {
+              boxShadow: 4,
+              transform: "translateY(-2px)",
+            },
+            width: 1,
+            maxWidth: 380,
+            transition: "all 0.2s ease-in-out",
+            mx: "auto",
+            border: isInCart
+              ? `1px solid ${theme.palette.primary.main}`
+              : "none",
+          }}
+        >
+          <ProductItemImage
+            product={product}
+            handleAddToCart={handleAddToCart}
+            isInCart={isInCart}
+            handleRemoveFromCart={handleRemoveFromCart}
+          />
 
-        <Stack spacing={1} sx={{ p: 2 }}>
-          <Typography variant="subtitle1" noWrap>
-            {product.name}
-          </Typography>
-
-          <Box
-            component="span"
-            sx={{
-              typography: "body2",
-              color: "text.secondary",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <Rating
-              name="product-rate"
-              value={product.rate || 0}
-              precision={0.5}
-              readOnly
-              sx={{ fontSize: "1.25rem" }}
-            />
-            <Typography component="span" variant="caption" sx={{ ml: 0.5 }}>
-              ({product.popularity} reviews)
+          <Stack spacing={1} sx={{ p: 2 }}>
+            <Typography variant="subtitle1" noWrap>
+              {product.name}
+              <Typography
+                component="div"
+                variant="caption"
+                color="text.secondary"
+                sx={{ whiteSpace: "normal", wordWrap: "break-word" }}
+              >
+                ({product.category})
+              </Typography>
             </Typography>
-          </Box>
 
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-          >
+            <Box
+              component="span"
+              sx={{
+                typography: "body2",
+                color: "text.secondary",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Rating
+                name="product-rate"
+                value={product.rate || 0}
+                precision={0.5}
+                readOnly
+                sx={{ fontSize: "1.25rem" }}
+              />
+              <Typography component="span" variant="caption" sx={{ ml: 0.5 }}>
+                ({product.popularity} reviews)
+              </Typography>
+            </Box>
+
             <Stack
               direction="row"
-              spacing={0.5}
-              sx={{ typography: "subtitle1" }}
+              alignItems="center"
+              justifyContent="space-between"
             >
-              <Box component="span">${product.price}</Box>
+              <Stack
+                direction="row"
+                spacing={0.5}
+                sx={{ typography: "subtitle1" }}
+              >
+                <Box component="span">${product.price}</Box>
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </Card>
+        </Card>
+      </Tooltip>
     </animated.div>
   );
 };
